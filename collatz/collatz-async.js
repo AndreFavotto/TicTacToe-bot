d266ffd2e44715bf264ff8ec59fd7dee744e0f7a
andrefavotto <andrefavotto@hotmail.com>
2023-02-10 23:23:55 -0300

    Upload collatz sequence

---
+++ collatz/collatz-async.js
@@ -0,0 +1,45 @@
+//The Collatz Sequence - using asynchronous input
+
+const readline = require("readline");
+
+function getInput(){
+    return new Promise(resolve => {
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+        rl.question("Type an integer: ", (number) => {
+            resolve(number);
+            rl.close();
+        });
+    });
+}
+
+function collatz(number){
+    if (!(Number.isInteger(number)))
+        throw new Error("Invalid input!")
+    else if (number==1)
+        return
+
+    else if (number%2 == 0){
+        answ = Math.floor(number/2);
+        console.log(answ);
+        collatz(answ);
+    }
+    else{
+        answ = 3 * number + 1;
+        console.log(answ);
+        collatz(answ);
+    }
+}
+
+async function run(){
+    const input = await getInput();
+    try{
+        collatz(Number(input));
+    } catch (e){
+        console.error(e);
+    }
+}
+
+run();
---
+++ collatz/collatz-sync.js
@@ -0,0 +1,32 @@
+//The Collatz Sequence - using synchronous input
+
+const prompt = require('prompt-sync')();
+
+function collatz(number){
+    if (!(Number.isInteger(number)))
+        throw new Error("Invalid input!")
+    else if (number==1)
+        return
+
+    else if (number%2 == 0){
+        answ = Math.floor(number/2);
+        console.log(answ);
+        collatz(answ);
+    }
+    else{
+        answ = 3 * number + 1;
+        console.log(answ);
+        collatz(answ);
+    }
+}
+
+function run(){
+    const input = prompt("Type an integer: ");
+    try{
+        collatz(Number(input));
+    } catch (e){
+        console.error(e);
+    }
+}
+
+run();
